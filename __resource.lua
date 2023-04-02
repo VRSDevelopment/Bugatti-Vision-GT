12cc44c2e0963baedca4536160ab20f67153fe4a
VRS Development <89213812+Arpanbarai21@users.noreply.github.com>
2023-04-03 01:54:10 +0530

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,22 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+author 'MR VIRUS'
+description 'Bugatti Vision GT'
+
+files {
+	'vehicles.meta',
+	'carvariations.meta',
+	'carcols.meta',
+	'handling.meta',
+	'caraddoncontentunlocks.meta',
+	'dlctext.meta',
+	'vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'caraddoncontentunlocks.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'dlctext.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
